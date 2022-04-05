daa2d55f38f74e1dfa7f161e2eb2a85bb2d7c094
Dali47 <meftah.dali@gmail.com>
2022-04-05 17:44:26 +0100

    HomePage

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,Image, Text, View } from 'react-native';
+import { TouchableHighlight, Button, Image,  StyleSheet, Text, View } from 'react-native';
+import ImageBackground from 'react-native/Libraries/Image/ImageBackground';
+
+import backImg from './assets/Background.jpg'; 
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+           <ImageBackground source={backImg} resizeMode="cover" style={styles.image}></ImageBackground>
+
+         <View style={styles.rectangle}>
+            <View style={styles.fixToText}>
+
+                <TouchableHighlight
+                   style={styles.submitNormal}
+                    underlayColor='#fff'>
+                  <Text style={styles.submitTextBlack}>LOG IN</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                   style={styles.submitOutline}
+                    underlayColor='#fff'>
+                  <Text style={styles.submitTextWhite}>REGISTER</Text>
+                </TouchableHighlight>
+
+
+            </View>
+            <View style={styles.line}>
+
+            </View>
+         </View> 
+         
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  image: {
+    justifyContent: "center",
+    flex: 5,
+  },
+  rectangle: {
+    backgroundColor: "white",
+    flex: 1,
+    
+  },
+  fixToText: {
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  submitOutline: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingRight: 45,
+    paddingLeft: 45,
+    backgroundColor: '#000',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
+  submitNormal: {
+    marginRight: 5,
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingRight: 50,
+    paddingLeft: 50,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#000',
+  },
+  submitTextWhite: {
+    color: '#fff',
+    textAlign: 'center',
+    fontFamily: 'Roboto',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  submitTextBlack: {
+    color: '#000',
+    textAlign: 'center',
+    fontFamily: 'Roboto',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+
+   line: {
+    width: 170,
+    height: 7,
+    backgroundColor: "black",
+    borderRadius: 10,
+    alignItems: 'center',
+    marginLeft: 95,
+    marginBottom: 5,
   },
 });
