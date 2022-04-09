a479954e215ef74382a96abf061c882e7e611f69
Dali47 <meftah.dali@gmail.com>
2022-04-09 18:09:05 +0100

    Adding login/Register

--- components/LogIn.js
+++ components/LogIn.js
@@ -1,23 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { TouchableHighlight, Button, Image,  StyleSheet, Text, View } from 'react-native';
-import ImageBackground from 'react-native/Libraries/Image/ImageBackground';
-import React from 'react';
+import React, {useEffect } from 'react';
 
 
-export default class LogIn extends React.Component {
-render(){
-    return(
-        <View style={styles.container}>
-            <Text>
-                Hello From Login Page
-            </Text>
-        </View>
+import * as AuthSession from 'expo-auth-session';
+import jwtDecode from 'jwt-decode';
+import { Button,View,Alert, Platform, StyleSheet, Image } from 'react-native';
+
+// you need to swap out these details with your auth0 credientals
+const auth0ClientId = "lkXp0Z1G9G1F57G6XscStcOyEG1L2qld";
+const authorizationEndpoint = "https://dev-tic1h7u8.us.auth0.com/authorize";
+
+
+const useProxy = Platform.select({ web: false, default: true });
+const redirectUri = AuthSession.makeRedirectUri({ useProxy });
+
+console.log(redirectUri)  // <-- you will need to add this to your auth0 callbacks / logout configs
+
+
+
+export default function LogIn () { 
+
+    const [request, result, promptAsync] = AuthSession.useAuthRequest(
+      {
+        redirectUri,
+        clientId: auth0ClientId,
+        // id_token will return a JWT token
+        responseType: 'id_token',
+        // retrieve the user's profile
+        scopes: ['openid', 'profile', 'email'],
+        extraParams: {
+          // ideally, this will be a random value
+          nonce: 'nonce',
+        },
+      },
+      { authorizationEndpoint }
     );
+  useEffect(() => {
+    promptAsync({ useProxy });
+  
+})
+      return null;
+  
 }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
