347ec9a481690a00aa5844b79ecb2829fa4fb1d6
Dali47 <meftah.dali@gmail.com>
2022-06-06 15:52:33 +0100

    Getting data and showing/onPress it with flatlist

--- components/History.js
+++ components/History.js
@@ -1,23 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { TouchableHighlight, Button, Image,  StyleSheet, Text, View } from 'react-native';
-import ImageBackground from 'react-native/Libraries/Image/ImageBackground';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+    image:'https://res.cloudinary.com/dhncrtnjp/image/upload/v1654164703/male_man_people_person_avatar_white_tone_icon_159363_ebtiac.png',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+    image:'https://res.cloudinary.com/dhncrtnjp/image/upload/v1654164703/male_man_people_person_avatar_white_tone_icon_159363_ebtiac.png',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+    image:'https://res.cloudinary.com/dhncrtnjp/image/upload/v1654164703/male_man_people_person_avatar_white_tone_icon_159363_ebtiac.png',
+  },
+];
 
-export default class History extends React.Component {
-render(){
-    return(
-        <View style={styles.container}>
-            <Text>
-                Hello From History Page
-            </Text>
-        </View>
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+  <Image source={{uri:item.image,}} style={{width: 45, height: 45}} />
+    <Text style={[styles.title, textColor]}>{item.title}</Text>
+  </TouchableOpacity>
+);
+
+const History = () => {
+  const [selectedId, setSelectedId] = useState(null);
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.id === selectedId ? 'white' : 'black';
+    item.id === selectedId ? alert(item.id) : console.log("ff");
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
+      />
     );
-}
-}
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: "row",
+  },
+  title: {
+    fontSize: 32,
+    marginLeft: 10,
   },
 });
+
+export default History;
--- components/Tabs.js
+++ components/Tabs.js
@@ -5,7 +5,7 @@ import { Image } from 'react-native';
 
 import Map from '../components/Map';
 import Reserve from '../components/Reserve';
-import Chat from '../components/Chat';
+import History from '../components/History';
 import Profile from '../components/Profile';
 import LogOut from '../components/LogOut';
 
@@ -17,7 +17,7 @@ export default function Tabs() {
     <NavigationContainer independent={true}>
       <Tab.Navigator screenOptions={{ headerShown: false }}>
         <Tab.Screen name="Map" component={Map} options={{tabBarIcon: () => (<Image source={{uri:'https://res.cloudinary.com/dhncrtnjp/image/upload/v1650120729/map_xjciba.png',}} style={{width: 31, height: 31}} />)}}/>
-        <Tab.Screen name="Chat" component={Chat} options={{tabBarIcon: () => (<Image source={{uri:'https://res.cloudinary.com/dhncrtnjp/image/upload/v1650121234/message_bubble_chat_icon_154003_ygz8mw.png',}} style={{width: 45, height: 45}} />)}}/>
+        <Tab.Screen name="History" component={History} options={{tabBarIcon: () => (<Image source={{uri:'https://res.cloudinary.com/dhncrtnjp/image/upload/v1650121234/message_bubble_chat_icon_154003_ygz8mw.png',}} style={{width: 45, height: 45}} />)}}/>
         <Tab.Screen name="Reserve" component={Reserve} options={{tabBarIcon: () => (<Image source={{uri:'https://res.cloudinary.com/dhncrtnjp/image/upload/v1654087487/reserver_hdpvl1.png',}} style={{width: 60, height: 30}} />)}}/>
         <Tab.Screen name="Profile" component={Profile} options={{tabBarIcon: () => (<Image source={{uri:'https://res.cloudinary.com/dhncrtnjp/image/upload/v1650121467/user_avatar_profile_person_account_icon_197530_echqw3.png',}} style={{width: 40, height: 40}} />)}}/>
         <Tab.Screen name="LogOut" component={LogOut} options={{tabBarIcon: () => (<Image source={{uri:'https://res.cloudinary.com/dhncrtnjp/image/upload/v1650120729/logout-2032031-1713022_xokf1v.png',}} style={{width: 40, height: 40}} />)}}/>
